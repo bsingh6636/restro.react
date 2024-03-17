6a00f6c022a10c5dca4e846301bba33e811fd759
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2024-03-17 23:53:29 +0530

    prettier code format

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 const heading = React.createElement('div',{id:'parent',abc:'xyz' , style:{color:'black'}},
     [React.createElement('div',{id:'child1'},   
         [React.createElement('h1',{},'hh1 inside child1'),React.createElement('h2',{},'h2 inside child1')]),
     React.createElement('div',{id:'child2'},[
         React.createElement('h1',{},'h1 inside child2'),React.createElement('h2',{},'h2 inside child2')])
-    ]);
+       ]);
 
 const root =ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
