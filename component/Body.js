e7d897c7313e86e82f62aa59b2527864aee705d2
Brijesh Singh <bsingh6636@gmail.com>
2024-03-26 17:19:30 +0530

    changes

--- component/Body.js
+++ component/Body.js
@@ -1,9 +1,10 @@
 import { Restrocard } from "./Restrocard";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Shimmer } from "./Shimmer";
 export const Body = () => {
   const [restrolist, setrestrolist] = useState([]);
-  const [searchvalue, setsearchvalue] = useState([]);
+  const [searchvalue, setsearchvalue] = useState("");
+  console.log(searchvalue);
   const filterestro = () => {
     let filteredlist = restrolist.filter((resti) => resti.info.avgRating > 4);
     setrestrolist(filteredlist);
@@ -23,13 +24,30 @@ export const Body = () => {
         jsone.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
     );
   };
+  console.log(restrolist)
   return restrolist.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
       <div className="filterclass">
-        <input type="text" className="searchtext"></input>
-        <button>Search</button>
+        <input
+          type="text"
+          className="searchtext"
+          value={searchvalue}
+          onChange={(e) => setsearchvalue(e.target.value)}
+          placeholder="Enter text"
+        ></input>
+        <button
+          onClick={() => {
+            let searchedrestro = restrolist.filter((res) =>
+              // res.name.includes(searchvalue)
+              res.info.name && typeof res.info.name === 'string' && res.info.name.includes(searchvalue)
+            );
+            setrestrolist(searchedrestro);
+          }}
+        >
+          Search
+        </button>
         <button type="button" className="filterbutton" onClick={filterestro}>
           Filter restro
         </button>
